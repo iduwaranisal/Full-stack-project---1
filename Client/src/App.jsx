9a387f4e14c0bc171d4813fe8324b8d0cc1a192f
Iduwara nisal <induwaranisal7158980@gmail.com>
2026-01-10 18:01:42 +0530

    complete 1 hour

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Cardetails from './pages/Cardetails';
+import Car from './pages/Car';
+import Mybookings from './pages/Mybookings';
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -8,7 +12,15 @@ const App = () => {
 
   return (
     <>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/car-details/:id' element={<Cardetails />} />
+        <Route path='/cars' element={<Car />} />
+        <Route path='/my-bookings' element={<Mybookings />} />
+      </Routes>
+
     </>
   )
 }
