a96758aeedadbf77b66e6807daa4792b2debc4d4
Iduwara nisal <induwaranisal7158980@gmail.com>
2026-01-11 16:12:09 +0530

    do 2.30 complete

--- Client/src/pages/Cardetails.jsx
+++ Client/src/pages/Cardetails.jsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyCarData } from '../assets/assets'
 import Loader from '../components/Loader'
+const currency = import.meta.env.VITE_CURRENCY
 
 const Cardetails = () => {
   const { id } = useParams()
   const navigate = useNavigate()
   const [car, setcar] = useState(null)
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    
+  }
 
   useEffect(() => {
     const foundCar = dummyCarData.find((item) => item._id === id)
@@ -15,7 +21,7 @@ const Cardetails = () => {
 
   return car ? (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
-      {/* Back Button */}
+
       <button onClick={() => navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer'>
         <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65' />
         Back to all cars
@@ -33,7 +39,7 @@ const Cardetails = () => {
 
             <hr className='border-borderColor my-6' />
 
-            {/* Car Specs Grid */}
+
             <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
               {[
                 { icon: assets.users_icon, text: `${car.seating_capacity} Seats` },
@@ -48,13 +54,13 @@ const Cardetails = () => {
               ))}
             </div>
 
-            {/* Description Section */}
+
             <div>
               <h1 className='text-xl font-medium mb-3'>Description</h1>
               <p className='text-gray-500 leading-relaxed'>{car.description}</p>
             </div>
 
-            {/* Features Section */}
+
             <div>
               <h1 className='text-xl font-medium mb-3'>Features</h1>
               <ul className='grid grid-cols-1 sm:grid-cols-2 gap-2'>
@@ -68,10 +74,49 @@ const Cardetails = () => {
             </div>
 
           </div>
-          <form>
-            
-          </form>
+
+
+
+
         </div>
+
+        <form onSubmit={handlesubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+
+          <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>
+            {currency}{car.pricePerDay}
+            <span className='text-base text-gray-400 font-normal'>per day</span>
+          </p>
+
+          <hr className='border-borderColor my-6' />
+
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="pickup-date" className="block mb-1">Pickup Date</label>
+            <input
+              type="date"
+              id="pickup-date"
+              className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+              min={new Date().toISOString().split("T")[0]}
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="return-date" className="block mb-1">Return Date</label>
+            <input
+              type="date"
+              id="return-date"
+              className="border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <button className='w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>
+            Book Now
+          </button>
+
+          <p className='text-center text-sm'>
+            No credit card required to reserve
+          </p>
+
+        </form>
 
       </div>
     </div>
